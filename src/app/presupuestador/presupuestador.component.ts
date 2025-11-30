630947eb00aec097c381322d46ab3df81e19ce56
carlos-martinez-maqueta <cmartinez.meneses1@gmail.com>
2025-11-30 10:56:26 -0500

    Presupuestador 1

--- src/app/presupuestador/presupuestador.component.ts
+++ src/app/presupuestador/presupuestador.component.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import Swal from 'sweetalert2';
 import { AuthService } from '../../service/auth.service';
 import { BreadcrumbComponent } from '../breadcrumb/breadcrumb.component';
+import { UserService } from '../../service/user.service';
 
 @Component({
   selector: 'app-presupuestador',
@@ -18,7 +19,27 @@ export class PresupuestadorComponent {
 
   title = 'Presupuestador';
 
+  locales: any[] = [];
+  constructor(private userService: UserService, private authService: AuthService) {}
+  
+  ngOnInit(): void {
+		this.loadLocales();
+	}
   onSubmit(): void {
 
   }
+
+  loadLocales(): void {
+    this.userService.getAllLocales().subscribe({
+      next: (res: any) => {
+        console.log("📌 Respuesta completa:", res); // <-- imprime toda la respuesta
+
+        this.locales = res.data || [];
+
+        console.log("📌 Locales cargados:", this.locales); // <-- imprime solo los locales
+      },
+      error: err => console.error("❌ Error al cargar locales:", err),
+    });
+  }
+
 }
